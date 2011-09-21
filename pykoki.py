6c6ddd156a61420259b6243bf4cac2eaf629573b
Chris Kirkham <chrisjameskirkham@gmail.com>
2011-09-21 18:36:02 +0100

    Added structure class for Point2Di, Point2Df, and Point3Df

--- pykoki.py
+++ pykoki.py
@@ -1,6 +1,34 @@
 from ctypes import *
 import os
 
+
+class Point2Di(Structure):
+
+    _fields_ = [("x", c_int), ("y", c_int)]
+
+    def __repr__(self):
+        return "Point2Di (x=%i, y=%i)" % (self.x, self.y)
+
+
+
+class Point2Df(Structure):
+
+    _fields_ = [("x", c_float), ("y", c_float)]
+
+    def __repr__(self):
+        return "Point2Df (x=%f, y=%f)" % (self.x, self.y)
+
+
+
+class Point3Df(Structure):
+
+    _fields_ = [("x", c_float), ("y", c_float), ("z", c_float)]
+
+    def __repr__(self):
+        return "Point3Df (x=%f, y=%f, z=%f)" % (self.x, self.y, self.z)
+
+
+
 class PyKoki:
 
     def __init__(self):
