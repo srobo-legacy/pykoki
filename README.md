a3e46506090b59b6a78bc8243228d85ffce4f7de
Rob Spanton <rspanton@zepler.net>
2012-10-29 17:09:18 +0000

    Add setup.py and instructions to use it
    Now pykoki can be installed with `./setup.py install`

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Requirements
 
  * [libkoki](https://www.studentrobotics.org/trac/wiki/libkoki)
  * [v4l2](http://pypi.python.org/pypi/v4l2)
+
+Installation
+------------
+
+pykoki can be installed used python distutils like this:
+
+~~~~~~~~~~~~
+% ./setup.py install
+~~~~~~~~~~~~
+
+That's it!
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup( name = "pykoki", 
+       version = "0.0.1",
+       requires = ["ctypes","v4l2",],
+       py_modules = ["pykoki"],
+
+       author = "Chris Kirkham",
+       author_email = "chrisjameskirkham@gmail.com",
+       url="https://www.studentrobotics.org/trac/wiki/libkoki",
+)
