af228802a62ba8cc9ab37adf7c346d4552808f7d
Rob Spanton <rob@robspanton.com>
2014-10-22 23:31:40 +0100

    setup.py: Remove ctypes from install_requires
    ctypes is provided in the python standard library

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup( name = "pykoki", 
        version = "0.0.1",
-       requires = ["ctypes","v4l2",],
+       install_requires = ["v4l2",],
        packages = ["pykoki"],
 
        author = "Chris Kirkham",
